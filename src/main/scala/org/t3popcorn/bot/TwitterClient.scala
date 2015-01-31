740f3b49f456100f580149b499e9e8e58e44b082
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-01-31 19:52:05 +0100

    Fixed twitter credentials

--- src/main/scala/org/t3popcorn/bot/TwitterClient.scala
+++ src/main/scala/org/t3popcorn/bot/TwitterClient.scala
@@ -1,13 +1,21 @@
 package org.t3popcorn.bot
 
 import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
 
 object SearchStreamer {
   def main(args: Array[String]) {
 
     val searchTerms = Array("t3popcorn", "typo3")
 
-    val twitterStream = new TwitterStreamFactory().getInstance
+    val cb = new ConfigurationBuilder()
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey(sys.env("consumerKey"))
+      .setOAuthConsumerSecret(sys.env("ConsumerSecret"))
+      .setOAuthAccessToken(sys.env("accessToken"))
+      .setOAuthAccessTokenSecret(sys.env("accessTokenSecret"))
+    val twitterStream = new TwitterStreamFactory(cb.build()).getInstance()
+
     twitterStream.addListener(Util.simpleStatusListener)
     twitterStream.filter(new FilterQuery().track(searchTerms))
     println(twitterStream)
