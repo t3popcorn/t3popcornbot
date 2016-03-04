84d03d26437fd17d853dab0f39c461b47c8a8985
ipf <ipf@users.noreply.github.com>
2016-03-04 13:46:28 +0100

    Update Util.scala

--- src/main/scala/org/t3popcorn/bot/Util.scala
+++ src/main/scala/org/t3popcorn/bot/Util.scala
@@ -16,7 +16,7 @@ object Util extends TwitterInstance {
         !Settings.blackListedUsers.contains(status.getUser.getScreenName) &&
         !Settings.environment.equals("dev")
 
-      if (replyOn && Random.nextInt(450) == 45) {
+      if (replyOn && Random.nextInt(450) == 62) {
         val reply = Random.shuffle(Settings.replies).head
 
         logger.info(status.getText)
